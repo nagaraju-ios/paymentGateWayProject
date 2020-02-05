a42b45c021a1f271b88061ab3ccf5d06b7e10d68
nagaraju <naga.thota99@gmail.com>
2020-02-05 17:02:41 +0530

    some changes

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'paymentGateWayProject' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for paymentGateWayProject
+
+
+  pod 'razorpay-pod'
+end
--- paymentGateWayProject/ViewController.swift
+++ paymentGateWayProject/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Razorpay
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,RazorpayPaymentCompletionProtocol{
+    
+    
+    var rozarpaystored:Razorpay!
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
+         rozarpaystored = Razorpay.initWithKey("rzp_live_3Sk6HSF3G1ITkl",andDelegate:self)
+        //showPaymentForm()
         // Do any additional setup after loading the view.
     }
 
 
-}
+    @IBAction func showPaymentBtn(_ sender: Any) {
+        
+       showPaymentForm()
+        
+    }
+    func onPaymentError(_ code: Int32, description str: String) {
+        print("error")
+    }
+    
+    func onPaymentSuccess(_ payment_id: String) {
+        print("sucess")
+    }
 
+ func showPaymentForm(){
+    let options: [String:Any] = [                "amount": "100",
+        "currency": "INR",               "description": "purchase description",                "order_id": "inv_ED2iH5m0XRDNYC",                "image": "https://url-to-image.png",                "name": "business or product name","prefill": [                    "contact": "9652763276",                    "email": "foo@bar.com"],                "theme": [ "color": "#F37254"              ]
+    ]
+    rozarpaystored.open(options)
+}
+}
